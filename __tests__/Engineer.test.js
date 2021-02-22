f7c4700e3a05488ffe22c9eb83e5e5a9ed95aeff
Lionel Townsend <townsendlionel@gmail.com>
2021-02-21 20:16:04 -0600

    added Engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,10 @@
-test.todo('should test engineer');
+const Engineer = require('../lib/Engineer')
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer('testName', 42, 'email@someplace.com', 'githubUser');
+
+    expect(engineer.getName).toBe('testName');
+    expect(engineer.getId).toBe(42);
+    expect(engineer.getEmail).toEqual(expect.any(String));
+    expect(engineer.getGithubUserName).toBe('githubUser');
+});
