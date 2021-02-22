d153c90b3650924c489b626fe68f4481badd5209
Lionel Townsend <townsendlionel@gmail.com>
2021-02-22 14:36:44 -0600

    fleshed out manager model and manager tests, added some jsdoc type annotations to the model class constructors

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,12 @@
-test.todo('should test manager');
+const Manager = require('../lib/Manager')
+
+test('creates a manager object', () => {
+    const manager = new Manager('testName', 42, 'email@someplace.com', 3);
+
+    expect(manager.getName).toBe('testName');
+    expect(manager.getId).toBe(42);
+    expect(manager.getEmail).toBe('email@someplace.com');
+    expect(manager.getOfficeNumber).toEqual(3);
+
+
+});
