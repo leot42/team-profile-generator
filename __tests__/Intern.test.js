86ba8261247d3ebab242d5766e6bb3a64091a8c1
Lionel Townsend <townsendlionel@gmail.com>
2021-02-22 20:46:08 -0600

    added intern model and intern prop tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,10 @@
-test.todo('should test intern');
+const Intern = require('../lib/Intern');
+
+test('creates an intern object', () => {
+    const intern = new Intern('testName', 42, 'email@someplace.com', 'Ridgemont High');
+
+    expect(intern.getName).toBe('testName');
+    expect(intern.getId).toEqual(42);
+    expect(intern.getEmail).toEqual(expect.any(String));
+    expect(intern.getSchool).toEqual('Ridgemont High');
+});
